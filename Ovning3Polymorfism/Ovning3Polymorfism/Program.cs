b77baaf1b7bc990e4a3abd827dcd6470a5df2a9f
k-sten <stenen@gmail.com>
2020-03-21 20:28:51 +0100

    1

---
+++ Ovning3Polymorfism/Ovning3Polymorfism/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ovning3Polymorfism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumericInputError numericInputError = new NumericInputError();
+            TextInputError textInputError = new TextInputError();
+            CharInputError charInputError = new CharInputError();
+            SymbolInputError symbolInputError = new SymbolInputError();
+            InputError inputerror = new InputError();
+            List<UserError> userErrors = new List<UserError> { numericInputError, textInputError, charInputError, symbolInputError, inputerror };
+
+            foreach (var error in userErrors)
+            {
+                Console.WriteLine(error.UEMessage());
+            }
+
+            // 3.4.11 Det låter oss definiera en basklass som subklasser kan ärva från som kan implementeras eller använda override
+
+            // 3.4.12 Istället för att behöva skriva samma kod flera gånger kan vi skriva basklassen och sedan använda oss av subklasser
+
+            // 3.4.13 En abstrakt klass definierar vad nånting är. Ett interface definierar vad nånting kan göra.
+            // Det går inte att ärva från olika typer av abstrakta klasser men en klass kan innehålla flera interface.
+        }
+    }
+}
---
+++ Ovning3Polymorfism/Ovning3Polymorfism/UserError.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ovning3Polymorfism
+{
+    abstract class UserError
+    {
+        public abstract string UEMessage();
+    }
+
+    class NumericInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a numeric input in a text field only. This fired an error!";
+        }
+    }
+
+    class TextInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a text input in a numeric only field. This fired an error!";
+        }
+    }
+
+    class CharInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a string input in a character only field. This fired an error!";
+        }
+    }
+
+    class SymbolInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a symbol input in an alpha numeric only field. This fired an error!";
+        }
+    }
+
+    class InputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use an input in an output only field. This fired an error!";
+        }
+    }
+}
